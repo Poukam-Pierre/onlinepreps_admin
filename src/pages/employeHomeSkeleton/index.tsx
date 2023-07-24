a1c78b9602aa4077933857683ed494fbdb021d68
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-07-24 19:15:45 +0100

    update scrollable

--- src/pages/employeHomeSkeleton/index.tsx
+++ src/pages/employeHomeSkeleton/index.tsx
@@ -1,5 +1,6 @@
 // Made by Poukam Ngamaleu
 
+import Scrollbars, { positionValues } from 'react-custom-scrollbars'
 import EmployeSideBar from '../../components/sideBar/sideBarEmploye'
 import { useAuth } from '../../utils/context'
 import Header from '../../components/header'
@@ -17,9 +18,9 @@ function EmployeSkeleton() {
         <Header />
         <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 4fr' }}>
           <EmployeSideBar />
-          <Box>
+          <Scrollbars universal autoHide>
             <Outlet></Outlet>
-          </Box>
+          </Scrollbars>
         </Box>
       </>
     )
