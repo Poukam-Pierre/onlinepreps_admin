9f8ff5a3f82ed398de634a15c5d80ce0e2f16142
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-02-09 23:45:38 +0100

    setup statistic view by adding exact amount to provide to patrners

--- src/components/statistique/finance/index.tsx
+++ src/components/statistique/finance/index.tsx
@@ -1,15 +1,47 @@
 // Made by Poukam Ngamaleu
 
-import { Box, Typography } from '@mui/material'
+import { Box, Paper, Typography } from '@mui/material'
 import emptyImg from '../../../asset/empty.svg'
+import { useEffect, useState } from 'react'
+import Axios from 'axios'
 
 function Finances() {
+  const [amount, setAmount] = useState<number>(0)
+  useEffect(() => {
+    Axios.get(`${process.env.REACT_APP_URL_REMOTE_LINK}/admin/getProvidedAmount`)
+      .then((res) => {
+        if (res?.status === 200) {
+          setAmount(Math.trunc(res.data))
+        }
+      })
+      .catch((err) => {
+        return
+      })
+
+  }, [])
   return (
     <Box p={3} display="grid" rowGap="70px">
       <Typography variant="h4" color="#555">
         Finances
       </Typography>
-      <Box display="grid" justifyContent="center">
+      <Box p={3}>
+        <Paper sx={{ padding: '20px', width: '15rem', bgcolor: '#F5F0F0' }}>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            paddingBottom="20px"
+          >
+            <Typography variant="h5" color="#555">
+              Montant à pouvoir
+            </Typography>
+          </Box>
+          <Typography variant="h5" fontWeight="bold" textAlign='center'>
+            {amount}
+          </Typography>
+        </Paper>
+
+      </Box>
+      {/* <Box display="grid" justifyContent="center">
         <img
           src={emptyImg}
           alt="Page indisponible"
@@ -18,7 +50,7 @@ function Finances() {
         <Typography variant="caption" textAlign="center">
           Page non disponible
         </Typography>
-      </Box>
+      </Box> */}
     </Box>
   )
 }
