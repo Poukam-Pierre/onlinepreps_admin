e0076a7ffd621e78912ee1cd96d08226a9ab191b
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-09 18:29:10 +0100

    fix(app): #2 set up adminSkeleton

--- src/pages/adminHomeSkeleton/index.tsx
+++ src/pages/adminHomeSkeleton/index.tsx
@@ -1,10 +1,13 @@
 // Made by Poukam Ngamaleu
 
 import { Box } from '@mui/system'
-import Header from '../../components/header'
 import { Outlet } from 'react-router'
 import AdminSideBar from '../../components/sideBar/sideBarAdmin'
 import { useAuth } from '../../utils/context'
+import SideBar from '../../components/Layout/SideBar/SideBar'
+import Footer from '../../components/Layout/Footer/Footer'
+// import Header from '../../components/header'
+import Header from '../../components/Layout/Header/Header'
 
 function AdminSkeleton() {
   const {
@@ -13,15 +16,30 @@ function AdminSkeleton() {
 
   return (
     is_admin && (
-      <>
-        <Header />
-        <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 4fr' }}>
-          <AdminSideBar />
-          <Box>
-            <Outlet></Outlet>
-          </Box>
+      <Box sx={{
+        display: 'grid',
+        gridTemplateColumns: 'auto 1fr',
+        height: '100%',
+      }}>
+        <SideBar />
+        <Box sx={{
+          display: 'grid',
+          gridTemplateRows: 'auto 1fr auto'
+        }}>
+          <Header />
+          <Outlet></Outlet>
+          <Footer />
         </Box>
-      </>
+      </Box>
+      // <>
+      //   <Header />
+      //   <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 4fr' }}>
+      //     <AdminSideBar />
+      //     <Box>
+      //       <Outlet></Outlet>
+      //     </Box>
+      //   </Box>
+      // </>
     )
   )
 }
