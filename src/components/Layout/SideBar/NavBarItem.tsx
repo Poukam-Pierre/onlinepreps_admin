255f8bd2120a905b266ba9e6260cbd1649b49113
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-11 21:02:52 +0100

    feat(app): #7 add style throught navigation element

--- src/components/Layout/SideBar/NavBarItem.tsx
+++ src/components/Layout/SideBar/NavBarItem.tsx
@@ -1,6 +1,7 @@
-import { Box, Collapse, Typography } from "@mui/material";
+import { Box, Collapse, Tooltip, Typography } from "@mui/material";
 import { sideBarItem } from "./SideBar";
 import { theme } from "../../../utils/style/theme";
+import { useLocation } from "react-router-dom";
 
 interface navBarItemProps {
     navEl: sideBarItem
@@ -9,24 +10,40 @@ interface navBarItemProps {
 export default function NavBarItem({
     navEl: { label, icon, link }, isSideOpen }:
     navBarItemProps) {
+    const location = useLocation()
+    const isActive = link === location.pathname
     return (
-        <Box sx={{
-            display: 'grid',
-            gridTemplateColumns: 'auto 1fr',
-            alignItems: 'center',
-            columnGap: '10px',
-            padding: isSideOpen ? '0 20px' : 0
-        }}>
-            {icon}
-            <Typography
-                component={Collapse}
-                in={isSideOpen}
-                orientation="horizontal"
-                sx={{
-                    color: theme.palette.secondary.contrastText,
-                    size: '0.8rem',
-                }}
-            >{label}</Typography>
-        </Box>
+        <Tooltip
+            arrow
+            title={isSideOpen ? "" : label}
+            placement="right"
+        >
+            <Box sx={{
+                display: 'grid',
+                gridTemplateColumns: 'auto 1fr',
+                alignItems: 'center',
+                columnGap: isSideOpen ? '10px' : 0,
+                padding: isSideOpen ? '5px 20px' : '5px',
+                justifySelf: isSideOpen ? 'inherit' : 'center',
+                cursor: 'pointer',
+                background: isActive ? theme.palette.primary.dark : 'none',
+                borderRadius: '10px',
+                '&:hover': {
+                    background: !isActive ? theme.palette.primary.dark : 'none'
+                }
+
+            }}>
+                {icon}
+                <Typography
+                    component={Collapse}
+                    in={isSideOpen}
+                    orientation="horizontal"
+                    sx={{
+                        color: theme.palette.secondary.contrastText,
+                        size: '0.8rem',
+                    }}
+                >{label}</Typography>
+            </Box>
+        </Tooltip>
     );
 }
