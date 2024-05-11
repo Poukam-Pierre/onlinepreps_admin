a9e249d3b9cb44c519597f1077f534254dd71eb5
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-11 11:47:16 +0100

    feat(app): #6 set up function footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,9 +1,33 @@
-import { Box } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 
 export default function Footer() {
     return (
-        <Box>
-            I'm in footer section
+        <Box sx={{
+            display: 'flex',
+            alignItems: 'center',
+        }}>
+            <Typography
+                variant="body2"
+                paddingRight='5px'
+            >
+                Powered by
+            </Typography>
+            <Box >
+                <Link
+                    underline="hover"
+                    variant="body2"
+                    target="_blank"
+                    href='#'
+                    sx={{
+                        display: 'flex',
+                        paddingRight: '5px',
+                        alignItems: 'center'
+                    }}
+                >
+                    Poukam Tech
+                    <img src='' alt="" />
+                </Link>
+            </Box>
         </Box>
     );
 }
