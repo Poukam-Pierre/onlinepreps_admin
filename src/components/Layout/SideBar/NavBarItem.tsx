6f00ac50a80d2e0dda6a3b00b07df0a7274c0604
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-12 14:35:28 +0100

    feat(app): #7 add navigation for function to nav elements

--- src/components/Layout/SideBar/NavBarItem.tsx
+++ src/components/Layout/SideBar/NavBarItem.tsx
@@ -1,7 +1,7 @@
 import { Box, Collapse, Tooltip, Typography } from "@mui/material";
 import { sideBarItem } from "./SideBar";
 import { theme } from "../../../utils/style/theme";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface navBarItemProps {
     navEl: sideBarItem
@@ -11,6 +11,7 @@ export default function NavBarItem({
     navEl: { label, icon, link }, isSideOpen }:
     navBarItemProps) {
     const location = useLocation()
+    const navigate = useNavigate()
     const isActive = link === location.pathname
     return (
         <Tooltip
@@ -18,21 +19,23 @@ export default function NavBarItem({
             title={isSideOpen ? "" : label}
             placement="right"
         >
-            <Box sx={{
-                display: 'grid',
-                gridTemplateColumns: 'auto 1fr',
-                alignItems: 'center',
-                columnGap: isSideOpen ? '10px' : 0,
-                padding: isSideOpen ? '5px 20px' : '5px',
-                justifySelf: isSideOpen ? 'inherit' : 'center',
-                cursor: 'pointer',
-                background: isActive ? theme.palette.primary.dark : 'none',
-                borderRadius: '10px',
-                '&:hover': {
-                    background: !isActive ? theme.palette.primary.dark : 'none'
-                }
-
-            }}>
+            <Box
+                onClick={() => navigate(link)}
+                sx={{
+                    display: 'grid',
+                    gridTemplateColumns: 'auto 1fr',
+                    alignItems: 'center',
+                    columnGap: isSideOpen ? '10px' : 0,
+                    padding: isSideOpen ? '5px 20px' : '5px',
+                    justifySelf: isSideOpen ? 'inherit' : 'center',
+                    cursor: 'pointer',
+                    background: isActive ? theme.palette.primary.dark : 'none',
+                    borderRadius: '10px',
+                    '&:hover': {
+                        background: !isActive ? theme.palette.primary.dark : 'none'
+                    }
+                }}
+            >
                 {icon}
                 <Typography
                     component={Collapse}
@@ -42,7 +45,9 @@ export default function NavBarItem({
                         color: theme.palette.secondary.contrastText,
                         size: '0.8rem',
                     }}
-                >{label}</Typography>
+                >
+                    {label}
+                </Typography>
             </Box>
         </Tooltip>
     );
