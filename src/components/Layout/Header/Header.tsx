b393cad68e5adb609e0181d8a89d90ca1db3bb7b
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-12 14:34:39 +0100

    fix(app): #2 fixed link for breadcrumbs

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -49,7 +49,7 @@ export default function Header() {
         },
         {
             title: 'Management',
-            href: '/admin/manager'
+            href: '/admin/manage'
         },
         {
             title: 'Statistics',
