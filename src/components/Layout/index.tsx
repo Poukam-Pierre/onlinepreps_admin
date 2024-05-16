a478a68c5740708022ae8e10658e5eedf85418c0
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-16 14:50:21 +0100

    feat(app): #4 adjust layout consider employe and admin

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,54 @@
+import { Box } from "@mui/material";
+import SideBar from "./SideBar/SideBar";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import { useNavigate, Outlet, useLocation } from 'react-router-dom'
+import { useEffect } from "react";
+import { useAuth } from "../../utils/context";
+
+
+
+export default function Layout() {
+  const navigate = useNavigate()
+  const activeLocation = useLocation()
+  const {
+    userInfo: { is_employe, is_admin },
+  } = useAuth()
+
+  useEffect(() => {
+    if (is_employe) {
+      if (
+        activeLocation.pathname === '/'
+        ||
+        activeLocation.pathname === '/admin'
+      ) navigate('/');
+      navigate(activeLocation.pathname)
+    } else if (is_admin) {
+      if (
+        activeLocation.pathname === '/admin'
+        ||
+        activeLocation.pathname === '/'
+      ) navigate('/admin');
+      navigate(activeLocation.pathname);
+    } else navigate('/login')
+  }, [])
+
+  return (
+    <Box sx={{
+      display: 'grid',
+      gridTemplateColumns: 'auto 1fr',
+      height: '100svh',
+    }}>
+      <SideBar />
+      <Box sx={{
+        display: 'grid',
+        gridTemplateRows: 'auto 1fr auto'
+      }}>
+        <Header />
+        <Outlet></Outlet>
+        <Footer />
+      </Box>
+    </Box>
+
+  );
+}
