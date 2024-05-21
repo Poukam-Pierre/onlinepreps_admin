e7b461e38aed86dfa324848761ecca0ef180fd7f
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-21 18:24:46 +0100

    fix(app): #11 change value for xAxis and data

--- src/components/dashboard/adminDashboard/AdminSouscriptionDetails/charts/MonthCharts.tsx
+++ src/components/dashboard/adminDashboard/AdminSouscriptionDetails/charts/MonthCharts.tsx
@@ -25,21 +25,21 @@ export default function MonthCharts({ monthsDataFetched }: monthChartProps) {
                 margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
             >
                 <defs>
-                    <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+                    <linearGradient id="value" x1="0" y1="0" x2="0" y2="1">
                         <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
                         <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
                     </linearGradient>
                 </defs>
-                <XAxis dataKey="month" />
+                <XAxis dataKey="days" />
                 <YAxis />
                 <CartesianGrid strokeDasharray="3 3" />
                 <Tooltip />
                 <Area
                     type="monotone"
-                    dataKey="total"
+                    dataKey="value"
                     stroke="#8884d8"
                     fillOpacity={1}
-                    fill="url(#total)"
+                    fill="url(#value)"
                 />
             </AreaChart>
         </ResponsiveContainer>
