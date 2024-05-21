b06666b36926ab3ab4cd0ddd7104701eceac9eb7
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-21 15:59:31 +0100

    feat(app): #11 set chart for month view

---
+++ src/components/dashboard/adminDashboard/AdminSouscriptionDetails/charts/MonthCharts.tsx
@@ -0,0 +1,48 @@
+// Made by Poukam Ngamaleu
+import {
+    Area,
+    AreaChart,
+    CartesianGrid,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from 'recharts'
+import { souscriptionDataFetched } from '../SouscriptionDetails'
+
+interface monthChartProps {
+    monthsDataFetched: souscriptionDataFetched[]
+}
+
+export default function MonthCharts({ monthsDataFetched }: monthChartProps) {
+
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <AreaChart
+                width={730}
+                height={250}
+                data={monthsDataFetched}
+                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+            >
+                <defs>
+                    <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                    </linearGradient>
+                </defs>
+                <XAxis dataKey="month" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Area
+                    type="monotone"
+                    dataKey="total"
+                    stroke="#8884d8"
+                    fillOpacity={1}
+                    fill="url(#total)"
+                />
+            </AreaChart>
+        </ResponsiveContainer>
+    )
+}
+
