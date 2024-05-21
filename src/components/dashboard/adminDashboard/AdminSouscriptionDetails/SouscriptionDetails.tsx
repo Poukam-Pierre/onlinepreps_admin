58c64b4cdd6b2e673a49df0a5aa576bfe6b91839
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-21 15:58:47 +0100

    feat(app): #11 set chart for years view

--- src/components/dashboard/adminDashboard/AdminSouscriptionDetails/SouscriptionDetails.tsx
+++ src/components/dashboard/adminDashboard/AdminSouscriptionDetails/SouscriptionDetails.tsx
@@ -1,6 +1,8 @@
 import { Box, Typography } from "@mui/material";
 import ChartSouscriptionSwapped from "./ChartSouscriptionSwapped";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import YearCharts from "./charts/YearCharts";
+import MonthCharts from "./charts/MonthCharts";
 
 export interface fetchingDataSelection {
     type: string;
@@ -8,17 +10,122 @@ export interface fetchingDataSelection {
     period?: number;
 
 }
+export interface souscriptionDataFetched {
+    [key: string]: number | string;
+}
+interface souscriptionDatas {
+    data?: souscriptionDataFetched[],
+    loaded: boolean,
+}
+const monthsDataFetched: souscriptionDataFetched[] = [
+    {
+        day1: 1,
+        value: 2
+    },
+    {
+        day1: 5,
+        value: 20
+    },
+    {
+        day1: 7,
+        value: 10
+    },
+    {
+        day1: 8,
+        value: 8
+    },
+    {
+        day1: 9,
+        value: 15
+    },
+    {
+        day1: 10,
+        value: 20
+    },
+    {
+        day1: 12,
+        value: 20
+    },
+    {
+        day1: 18,
+        value: 7
+    },
+    {
+        day1: 25,
+        value: 2
+    },
+]
+const yearsDataFetched: souscriptionDataFetched[] = [
+    {
+        month: 'Jan',
+        total: 2
+    },
+    {
+        month: 'Feb',
+        total: 10
+    },
+    {
+        month: 'Mar',
+        total: 20
+    },
+    {
+        month: 'Apr',
+        total: 3
+    },
+    {
+        month: 'May',
+        total: 5
+    },
+    {
+        month: 'Jun',
+        total: 8
+    },
+    {
+        month: 'Jul',
+        total: 7
+    },
+    {
+        month: 'Aug',
+        total: 10
+    },
+    {
+        month: 'Sep',
+        total: 13
+    },
+    {
+        month: 'Oct',
+        total: 27
+    },
+    {
+        month: 'Nov',
+        total: 18
+    },
+    {
+        month: 'Dec',
+        total: 26
+    },
+
+]
 
 export default function SouscriptionDetails() {
     const [activePeriodSelection, setActivePeriodSelection] = useState<fetchingDataSelection>({
         type: "years",
         value: new Date().getFullYear(),
     })
+    const [souscriptionDatas, setSouscriptionDatas] = useState<souscriptionDatas>({
+        loaded: true
+    })
+    useEffect(() => {
+        // Fetch data fro the selection month and year
+        setSouscriptionDatas({
+            data: yearsDataFetched,
+            loaded: false
+        })
+    }, [activePeriodSelection])
 
     return (
         <Box sx={{
             backgroundColor: '#fff',
-            minWidth: '50%',
             height: 'auto',
             display: 'grid',
             rowGap: 2,
@@ -35,10 +142,22 @@ export default function SouscriptionDetails() {
                     setActivePeriodSelection={setActivePeriodSelection}
                 />
             </Box>
-            <Typography>
-                I'm here
-            </Typography>
-        </Box>
+            {activePeriodSelection.type === 'years' ? (
+                <Box sx={{
+                    height: 200,
+                    width: 'auto'
+                }}>
+                    <YearCharts yearsDataFetched={souscriptionDatas.data as souscriptionDataFetched[]} />
+                </Box>
 
+            ) : (
+                <Box sx={{
+                    height: 200,
+                    width: 'auto'
+                }}>
+                    <MonthCharts monthsDataFetched={souscriptionDatas.data as souscriptionDataFetched[]} />
+                </Box>
+            )}
+        </Box>
     );
 }
