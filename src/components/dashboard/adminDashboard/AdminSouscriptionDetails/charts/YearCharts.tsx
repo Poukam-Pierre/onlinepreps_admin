9bf2c717fa2a2b2f8e4c14897a5a993b69cfcdc3
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-21 15:59:57 +0100

    feat(app): #11 set charts for year view

---
+++ src/components/dashboard/adminDashboard/AdminSouscriptionDetails/charts/YearCharts.tsx
@@ -0,0 +1,63 @@
+// Made by Poukam Ngamaleu
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import { souscriptionDataFetched } from '../SouscriptionDetails'
+
+interface yearChartProps {
+  yearsDataFetched: souscriptionDataFetched[]
+}
+
+export default function YearCharts({ yearsDataFetched }: yearChartProps) {
+
+  // useEffect(() => {
+  //   Axios.get(
+  //     `${process.env.REACT_APP_URL_REMOTE_LINK}/admin/getSouscriptionDate`
+  //   )
+  //     .then((res) => {
+  //       if (res?.status === 200) {
+  //         setSouscriptionData(res.data)
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       if (err.response.status === 400) {
+  //         return
+  //       }
+  //     })
+  // }, [])
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <AreaChart
+        width={730}
+        height={250}
+        data={yearsDataFetched}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="month" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="total"
+          stroke="#8884d8"
+          fillOpacity={1}
+          fill="url(#total)"
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  )
+}
+
