2bb232451a1989b918d923ecd5839c1852050cec
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-23 17:12:50 +0100

    feat(app): #11 set up an overview details function begons to dashboard

---
+++ src/components/dashboard/adminDashboard/OverviewDetails/OverviewDetails.tsx
@@ -0,0 +1,107 @@
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Box, IconButton, Tooltip, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { theme } from "../../../../utils/style/theme";
+import { shortenNumber } from "../../../../utils/utilis/ShorterNumber";
+import MenuList from './MenuList';
+import BarCharts from './BarChart';
+import { partnersDetailsType } from '../../../../utils/context/partner/partner.interface';
+
+export interface testCategoryStat {
+    [key: string]: string | number;
+    category: string;
+    value: number
+}
+
+
+interface overviewDetailsProps {
+    label: string;
+    dataList: partnersDetailsType[];
+}
+
+export default function OverviewDetails({
+    label,
+    dataList,
+}: overviewDetailsProps) {
+    const [anchorEl, setAnchorEl] = useState<HTMLAnchorElement | null>(null)
+    const [selectedPartner, setSelectedPartner] = useState<partnersDetailsType>(dataList[0])
+    const { name, total, uniqueId, testCategory } = selectedPartner
+    useEffect(() => {
+        setSelectedPartner(dataList[0])
+    }, [dataList])
+    return (
+        <>
+            <MenuList
+                anchorEl={anchorEl}
+                setAnchorEl={setAnchorEl}
+                dataNameList={dataList.map(partner => partner.name)}
+                setPartnerSelected={setSelectedPartner}
+            />
+            <Box sx={{
+                display: "grid",
+                background: theme.palette.secondary.contrastText,
+                width: "fit-content",
+                padding: "10px",
+                borderRadius: "10px",
+            }}>
+                <Box sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                }}>
+                    <Box>
+                        <Typography variant="h6">{label} Details</Typography>
+                        <Typography variant="caption">{name}</Typography>
+                    </Box>
+                    <Tooltip
+                        arrow
+                        title="Partners list"
+                    >
+                        <IconButton
+                            size="small"
+                            sx={{
+                                alignSelf: "baseline"
+                            }}
+                            onClick={(event) => setAnchorEl(event.target as HTMLAnchorElement)}
+                        >
+                            <MoreHorizIcon />
+                        </IconButton>
+                    </Tooltip>
+                </Box>
+                <BarCharts dataSet={testCategory} />
+                <Box sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-evenly",
+                    gap: 2
+                }}>
+                    <Box sx={{
+                        width: "fit-content",
+                        border: "2px solid #0D3A4A",
+                        borderRadius: "5px",
+                        padding: "5px",
+                        flex: 1
+                    }}>
+                        <Typography variant="caption">Total students</Typography>
+                        <Typography
+                            variant="body1"
+                        >{shortenNumber(total)}</Typography>
+                    </Box>
+                    <Box sx={{
+                        width: "fit-content",
+                        border: "2px solid #0D3A4A",
+                        borderRadius: "5px",
+                        padding: "5px",
+                        flex: 1
+                    }}>
+                        <Typography variant="caption">Code Preps</Typography>
+                        <Typography
+                            variant="body1"
+                        >{uniqueId}</Typography>
+                    </Box>
+
+                </Box>
+            </Box>
+        </>
+    );
+}
