277a459b189b9fd91794dcedb78a5fe4901e0b44
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-25 23:39:47 +0100

    fix(app): #11 import provider at the root of project

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,18 @@ import './utils/language'
 import LanguageContextProvider from './utils/language/LanguageContextProvider'
 import PartnerDetailsContextProvider from './utils/context/partners/PartnerContextProvider'
 import AuthContextProvider from './utils/context/login/LoginContextProvider'
+import EmployesDetailsContextProvider from './utils/context/employes/EmployesContextProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <LanguageContextProvider>
     <AuthContextProvider>
       <PartnerDetailsContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <EmployesDetailsContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </EmployesDetailsContextProvider>
       </PartnerDetailsContextProvider>
     </AuthContextProvider>
   </LanguageContextProvider>
